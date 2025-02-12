992451936fc29c0f5a796a303e24c9a2bde2c151
Andrey Rakhmatullin <wrar@wrar.name>
2025-02-12 17:59:47 +0500

    Basic extraction.

---
+++ conftest.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from fake_zyte_api.main import make_app
+
+if TYPE_CHECKING:
+    from aiohttp.pytest_plugin import AiohttpClient, AiohttpServer
+    from aiohttp.test_utils import TestClient, TestServer
+    from aiohttp.web import Application, Request
+
+
+@pytest.fixture
+async def api_client(
+    aiohttp_client: AiohttpClient,
+) -> TestClient[Request, Application]:
+    app = make_app()
+    return await aiohttp_client(app)
+
+
+@pytest.fixture
+async def jobs_website(aiohttp_server: AiohttpServer) -> TestServer:
+    from zyte_test_websites.jobs.app import make_app
+    from zyte_test_websites.utils import get_default_data
+
+    app = make_app(get_default_data("jobs"))
+    return await aiohttp_server(app)
--- fake_zyte_api/api.py
+++ fake_zyte_api/api.py
@@ -1,5 +1,45 @@
+from __future__ import annotations
+
+from base64 import b64encode
 from typing import Any
 
+import aiohttp
+from itemadapter import ItemAdapter
+from web_poet import HttpResponse
+from zyte_common_items import ZyteItemAdapter
+from zyte_test_websites.jobs.extraction import (
+    TestJobPostingNavigationPage,
+    TestJobPostingPage,
+)
 
-def handle_request(data: dict[str, Any]) -> dict[str, Any]:
-    return {}
+ItemAdapter.ADAPTER_CLASSES.appendleft(ZyteItemAdapter)  # type: ignore[attr-defined]
+
+
+async def handle_request(request_data: dict[str, Any]) -> dict[str, Any]:
+    url = request_data["url"]
+    response_data: dict[str, Any] = {
+        "url": url,
+    }
+
+    async with aiohttp.ClientSession() as session, session.get(url) as resp:
+        website_response_body = await resp.read()
+
+    if "httpResponseBody" in request_data:
+        body_b64 = b64encode(website_response_body).decode()
+        response_data["httpResponseBody"] = body_b64
+
+    if "jobPostingNavigation" in request_data:
+        web_poet_response = HttpResponse(url, website_response_body)
+        job_posting_nav_page = TestJobPostingNavigationPage(web_poet_response)
+        job_posting_navigation = await job_posting_nav_page.to_item()
+        response_data["jobPostingNavigation"] = ItemAdapter(
+            job_posting_navigation
+        ).asdict()
+
+    if "jobPosting" in request_data:
+        web_poet_response = HttpResponse(url, website_response_body)
+        job_posting_page = TestJobPostingPage(web_poet_response)
+        job_posting = await job_posting_page.to_item()
+        response_data["jobPosting"] = ItemAdapter(job_posting).asdict()
+
+    return response_data
